bb32d0c1ed092e1e3033899a3215bf5f54d19051
Yuri Rezende <yuri.rezende@souunit.com.br>
2023-02-21 11:13:14 -0300

    Criando componentes da página Home

---
+++ src/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 110vh;  // 90% da tela
+    width: 110vh;
+    border-radius: 20px;
+    background: #2c2e3a;
+    padding: 70px 0;
+`;
