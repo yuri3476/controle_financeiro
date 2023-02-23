4708596659836ecc0194c60e4257c7926cd8c2af
jackson-ribeiro <jacksonquest1@gmail.com>
2023-02-23 14:24:26 -0300

    Página de Login (teste)

---
+++ src/Login.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [pageTitle] = useState("Controle Financeiro");
+
+  useEffect(() => {
+    document.title = pageTitle;
+  }, [pageTitle]);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <div className="login">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Usuário</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
