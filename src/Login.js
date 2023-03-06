fd0ff3f1d028e15f5f9a21bec53c2298b2112f86
jackson-ribeiro <jacksonquest1@gmail.com>
2023-03-06 19:21:49 -0300

    Campos Login e Esqueci a senha(Parte 01)

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import './Login.css';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [pageTitle] = useState("Controle Financeiro");
+  const [forgotPassword, setForgotPassword] = useState(false);
+  // const [rememberMe, setRememberMe] = useState(false);
 
   useEffect(() => {
     document.title = pageTitle;
@@ -17,10 +20,50 @@ function Login() {
     setPassword(event.target.value);
   };
 
+  // const handleRememberMeChange = (event) => {
+  //   setRememberMe(event.target.checked);
+  // };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(`Username: ${username}, Password: ${password}`);
   };
+
+  const handleForgotPassword = () => {
+    setForgotPassword(true);
+  };
+
+  const handleCancelForgotPassword = () => {
+    setForgotPassword(false);
+  };
+
+  const handleResetPassword = (email) => {
+    // enviar solicitação para redefinir a senha do usuário com o e-mail fornecido
+    console.log(`Redefinir senha para o e-mail: ${email}`);
+    setForgotPassword(false);
+  };
+
+  if (forgotPassword) {
+    return (
+      <div className="login">
+        <h2>Esqueceu sua senha?</h2>
+        <p>Insira seu e-mail para redefinir sua senha:</p>
+        <form onSubmit={(event) => { event.preventDefault(); handleResetPassword(event.target.email.value); }}>
+          <div className="form-group">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+          </div>
+          <button type="submit">Redefinir senha</button>
+          <button className="forgot-password-button" onClick={handleCancelForgotPassword}>Cancelar</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div className="login">
@@ -44,7 +87,21 @@ function Login() {
             onChange={handlePasswordChange}
           />
         </div>
+        {/* <div className="form-group">
+          <div className="checkbox-wrapper">
+          <input
+            type="checkbox"
+            id="remember-me"
+            checked={rememberMe}
+            onChange={handleRememberMeChange}
+          />
+          <label className="checkbox-label" htmlFor="remember-me">
+            Permanecer logado
+          </label>
+          </div>
+        </div> */}
         <button type="submit">Entrar</button>
+        <button className="forgot-password-button" onClick={handleForgotPassword}>Esqueci minha senha</button>
       </form>
     </div>
   );
